e074c737164b43157ee69b2bd76dbcd4dd0f16f1
minion0572 <sandybhammarkar55@gmail.com>
2025-09-01 16:31:15 +0530

    new updates

--- 01-09-2025/salary/salary_main.py
+++ 01-09-2025/salary/salary_main.py
@@ -1,16 +1,46 @@
 from salary_manager import create_salary , read_all ,read_by_salary
 from salary_manager import salaries , update , delete_by_salary
 
-create_salary(1000)
-create_salary(5000)
-create_salary(8000)
-create_salary(3000)
+def menu():
+    message = '''
+1 - Create Salary
+2 - Read All Salary
+3 - Read by Salary
+4 - Update
+5 - Delete
+6 - Exit / Logout
+'''
+    choice = int(input(message))
+    if choice == 1:
+        salary = int(input("Salary: "))
+        create_salary(salary)
+    elif choice == 2:
+        result_salaries = read_all()
+        print("Salaries: ")
+        for salary in result_salaries:
+            print(salary)
+    elif choice == 3:
+        salary = int(input("Search Salary"))
+        index = read_by_salary(salary)
+        if salary == -1:
+            print("Salary not Found")
+        else:
+            print(f'salary is at index{index}')
+    elif choice == 4:
+        old_salary = int(input("salary to update"))
+        new_salary = int(input("New Salary"))
+        update(salary , new_salary)
 
-result_salaries = read_all()
-for salary in salaries:
-    print(salary)
+    elif choice == 5:
+        salary = int(input("Salary to delete"))
+        delete_by_salary(salary)
+    return choice
 
-update(8000,8500)
-print(read_all())
-delete_by_salary(1000)
-print(read_all())
+def menus():
+    print("Salary management App")
+    choice = menu()
+    while choice != 6:
+        choice = menu()
+    print("Thank you for using app")
+
+menus()
